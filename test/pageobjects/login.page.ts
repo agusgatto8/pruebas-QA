5a8db35751c3aa5772dca31f56d2a1fc6904ffd4
Agustin <agustin.gatto@radiumrocket.com>
2023-06-15 17:27:16 -0300

    Finish

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -2,40 +2,36 @@ import { ChainablePromiseElement } from 'webdriverio';
 
 import Page from './page';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    public get inputUsername () {
-        return $('#username');
+
+    public get btnMenu() {
+        return $('//*[@id="main-header"]/div/div/div[1]/div/button');
+    }
+    public get btnAccept() {
+        return $('#didomi-notice-agree-button')
+    }
+    public get sidebar() {
+        return $('//*[@id="sideBar"]');
+    }
+    public get btnMarcket() {
+        return $('//*[@id="dropdownSideBar"]/div[1]/a');
+    }
+    public get btnworldIndex() {
+        return $('//*[@id="MercadosyCotizaciones"]/li[9]/a');
     }
 
-    public get inputPassword () {
-        return $('#password');
+    public get ibexSelector() {
+        return $('//*[@id="lazy-container"]  // td[contains(.,"IBEX 35")]/following-sibling::td[1]/descendant::span')
     }
-
-    public get btnSubmit () {
-        return $('button[type="submit"]');
+    public get beSelector() {
+        return $('//*[@id="lazy-container"]  // td[contains(.,"BE 20")]/following-sibling::td[1]/descendant::span')
     }
-
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    public async login (username: string, password: string) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
+    public get ukSelector() {
+        return $('//*[@id="lazy-container"]  // td[contains(.,"UK 100")]/following-sibling::td[1]/descendant::span')
     }
-
-    /**
-     * overwrite specific options to adapt it to page object
-     */
     public open () {
-        return super.open('login');
+        return super.open('');
     }
 }
 
--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,14 +1,32 @@
 import LoginPage from '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('Taking values', () => {
+    it('Click on menu button', async () => {
         await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        await LoginPage.btnAccept.click();
+        await LoginPage.btnMenu.click();
+        await browser.pause(1000)
+    });
+
+    it('Select Marcket', async () => {
+        await LoginPage.sidebar.waitForDisplayed();
+        await LoginPage.btnMarcket.click();
+        await LoginPage.btnworldIndex.waitForDisplayed();
+        await LoginPage.btnworldIndex.click();
+        await browser.pause(1000);
+    });
+
+
+    it('Check selectors', async () => {
+        await LoginPage.ibexSelector.waitForDisplayed();
+        const ibexValue = await LoginPage.ibexSelector.getText();
+        const beValue = await LoginPage.beSelector.getText();
+        const ukValue = await LoginPage.ukSelector.getText();
+        const valuesFinalValue = [parseInt(beValue), parseInt(ukValue), parseInt(ibexValue)];
+        valuesFinalValue.sort((a, b) => a - b);
+        console.log('Rodrigo HDP', await valuesFinalValue);
     });
 });
 
